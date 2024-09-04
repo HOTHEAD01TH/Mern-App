1992032bb1e7fe9b0b3096c904909010bc00c970
HOTHEAD01TH <hothead01th@gmail.com>
2024-09-04 22:26:26 +0530

    added some routes and created some pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,74 +1,20 @@
-import React, { useState } from 'react';
-import Todo from './Todo';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/home';
+import About from './pages/about';
+import Signup from './pages/signup';
+import Signin from './pages/signin';
+import Profile from './pages/profile';
 
-function TodoApp() {
-  const [todos, setTodos] = useState([
-    { id: 1, title: 'Todo 1', description: 'Description for Todo 1' },
-    { id: 2, title: 'Todo 2', description: 'Description for Todo 2' },
-    { id: 3, title: 'Todo 3', description: 'Description for Todo 3' }
-  ]);
-  const [newTodoTitle, setNewTodoTitle] = useState('');
-  const [newTodoDescription, setNewTodoDescription] = useState('');
-
-  const handleAddTodo = () => {
-    if (newTodoTitle.trim() !== '') {
-      const newTodo = {
-        id: todos.length + 1,
-        title: newTodoTitle,
-        description: newTodoDescription
-      };
-      setTodos([...todos, newTodo]);
-      setNewTodoTitle('');
-      setNewTodoDescription('');
-    }
-  };
-
-  const handleDeleteTodo = (id) => {
-    setTodos(todos.filter(todo => todo.id !== id));
-  };
-
-  const handleEditTodo = (id, newTitle, newDescription) => {
-    const updatedTodos = todos.map(todo => {
-      if (todo.id === id) {
-        return { ...todo, title: newTitle, description: newDescription };
-      }
-      return todo;
-    });
-    setTodos(updatedTodos);
-  };
-
+export default function App() {
   return (
-    <div>
-      <h1>Todo App</h1>
-      <div>
-        <input
-          type="text"
-          placeholder="Enter todo title"
-          value={newTodoTitle}
-          onChange={(e) => setNewTodoTitle(e.target.value)}
-        />
-        <input
-          type="text"
-          placeholder="Enter todo description"
-          value={newTodoDescription}
-          onChange={(e) => setNewTodoDescription(e.target.value)}
-        />
-        <button onClick={handleAddTodo}>Add Todo</button>
-      </div>
-      <ul>
-        {todos.map(todo => (
-          <li key={todo.id}>
-            <Todo
-              title={todo.title}
-              description={todo.description}
-              onDelete={() => handleDeleteTodo(todo.id)}
-              onEdit={(newTitle, newDescription) => handleEditTodo(todo.id, newTitle, newDescription)}
-            />
-          </li>
-        ))}
-      </ul>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="aboutus" element={<About />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
-
-export default TodoApp;
--- frontend/src/DeleteTodoButton.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function DeleteTodoButton({ onDelete }) {
-  return (
-    <button onClick={onDelete}>Delete</button>
-  );
-}
-
-export default DeleteTodoButton;

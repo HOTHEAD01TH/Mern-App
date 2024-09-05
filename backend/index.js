af81b334114ac11d1dfb0159bb1f108604d5a97d
HOTHEAD01TH <hothead01th@gmail.com>
2024-09-06 01:27:31 +0530

    working

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv'); // Change import to require
 
+dotenv.config(); // Load environment variables from .env file
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('connected to mongo');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
-app.listen(3000, () =>{
-console.log('server is running....');
-
-
+app.listen(3000, () => {
+  console.log('server is running....');
 });
